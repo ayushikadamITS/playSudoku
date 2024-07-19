42e8411ea0e89f0546c6f4e861fff25004d3de1a
Ayushi Kadam <ayushi.kadam@intelliatech.com>
2024-07-19 13:04:40 +0530

    initial sudoku commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import SudokuGame from "./Components/SudokuGame";
+import 'animate.css';
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/sudoku" element={<SudokuGame />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/sudoku");
+  };
+
+  return (
+    <>
+   <h1 class="animate__heartBeat">Fun Time With Sudoku..!!</h1>
+    <div className="Home">
+      <h3 class="animate__heartBeat">Welcome To The Game</h3>
+      <button onClick={handleButtonClick}>Let's Play</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
